f19a02656ae57384a6a69811612608ed4120b8cc
Kronos <kronos@brandmind.dev>
2026-04-27 18:55:16 -0700

    cleanup: remove debug endpoints

--- api/fba/debug-all-env.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const allKeys = Object.keys(process.env).sort();
-  res.json({ 
-    total: allKeys.length,
-    keys: allKeys,
-  });
-}
